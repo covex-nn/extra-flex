495b5e504d8ac8f2f3b5d90375baaaebc2681a2f
Andrey F. Mindubaev <covex.mobile@gmail.com>
2017-12-10 00:53:28 +0300

    Proof of Concept

---
+++ README.md
@@ -0,0 +1,12 @@
+Extra-Flex
+==========
+Composer plugin for Symfony Flex
+
+Demonstration:
+
+```
+composer create-project symfony/skeleton . ^4.0
+composer require covex-nn/extra-flex dev-master
+composer require covex-nn/extra-flex-foobar
+composer remove covex-nn/extra-flex-foobar
+```
--- src/Composer/ExtraFlex.php
+++ src/Composer/ExtraFlex.php
@@ -4,12 +4,15 @@ namespace Covex\Composer;
 
 use Composer\Composer;
 use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\OperationInterface;
 use Composer\DependencyResolver\Operation\UninstallOperation;
 use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Factory;
 use Composer\Installer\PackageEvent;
 use Composer\Installer\PackageEvents;
 use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
 use Composer\Package\PackageInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
@@ -65,8 +68,7 @@ class ExtraFlex implements PluginInterface, EventSubscriberInterface
                 break;
             }
         }
-
-        list($this->options, $this->lock) = \Closure::bind(function (Composer $composer, IOInterface $io) {
+        $this->options = \Closure::bind(function (Composer $composer, IOInterface $io) {
             if (isset($this)) {
                 $flex = $this;
             } else {
@@ -74,51 +76,38 @@ class ExtraFlex implements PluginInterface, EventSubscriberInterface
                 $flex->activate($composer, $io);
             }
 
-            return [ $flex->initOptions(), $flex->lock ];
-        }, null, Flex::class)->__invoke($this->composer, $this->io);
+            return $flex->initOptions();
+        }, $flex, Flex::class)->__invoke($this->composer, $this->io);
 
+        $this->lock = new Lock(str_replace(Factory::getComposerFile(), 'composer.json', 'extra-flex.lock'));
         $this->configurator = new Configurator($composer, $io, $this->options);
-
-        $io->writeError("ExtraFlex is activated");
     }
 
-    /**
-     * Record events
-     *
-     * @param PackageEvent $event Package event
-     */
-    public function record(PackageEvent $event)
+    public function update(PackageEvent $event)
     {
         $operation = $event->getOperation();
-        /** @var InstallOperation|UninstallOperation|UpdateOperation $operation */
-        if ($operation instanceof UpdateOperation) {
-            $package = $operation->getTargetPackage();
-        } else {
+        /** @var OperationInterface $operation */
+        if ($operation instanceof InstallOperation || $operation instanceof UninstallOperation) {
             $package = $operation->getPackage();
+        } else {
+            return;
         }
-        $shouldRecord = false;
-        $name = $package->getName();
-        /** @var PackageInterface $package */
-        if ($operation instanceof InstallOperation) {
-            if (!$this->lock->has($name)) {
-                $shouldRecord = true;
+        $name = $package->getPrettyName();
+
+        $recipe = $this->createRecipe($package, $operation->getJobType());
+        if ($recipe instanceof Recipe) {
+            if ($operation instanceof InstallOperation && !$this->lock->has($name)) {
+                $this->configurator->install($recipe);
+
+                $this->lock->add($name, $package->getPrettyVersion());
+                $this->lock->write();
+            } elseif ($operation instanceof UninstallOperation && $this->lock->has($name)) {
+                $this->configurator->unconfigure($recipe);
+
+                $this->lock->remove($name);
+                $this->lock->write();
             }
-        } elseif ($operation instanceof UninstallOperation) {
-            $shouldRecord = true;
         }
-        if ($shouldRecord) {
-            $this->io->writeError("[ExtraFlex::record] " . $operation->getJobType() . " " . $name);
-        }
-    }
-
-    public function install(Event $event)
-    {
-        $this->io->writeError("[ExtraFlex::install]");
-    }
-
-    public function update(Event $event)
-    {
-        $this->io->writeError("[ExtraFlex::update]");
     }
 
     /**
@@ -127,11 +116,83 @@ class ExtraFlex implements PluginInterface, EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            PackageEvents::POST_PACKAGE_INSTALL => 'record',
-            PackageEvents::POST_PACKAGE_UPDATE => 'record',
-            PackageEvents::POST_PACKAGE_UNINSTALL => 'record',
-            ScriptEvents::POST_INSTALL_CMD => 'install',
-            ScriptEvents::POST_UPDATE_CMD => 'update',
+            PackageEvents::POST_PACKAGE_INSTALL => 'update',
+            // PackageEvents::POST_PACKAGE_UPDATE => 'update',
+            PackageEvents::PRE_PACKAGE_UNINSTALL => 'update',
         ];
+    }
+
+    /**
+     * @param PackageInterface $package
+     * @param string           $job
+     *
+     * @return Recipe|null
+     */
+    protected function createRecipe(PackageInterface $package, $job)
+    {
+        $extra = $package->getExtra();
+        if (!isset($extra["recipe-dir"])) {
+            return null;
+        }
+
+        $name = $package->getPrettyName();
+        $version = $package->getPrettyVersion();
+
+        $path = $this->composer->getInstallationManager()->getInstallPath($package) . "/" .
+            trim($extra["recipe-dir"], "\\/");
+        $path = str_replace("\\", "/", $path);
+
+        $manifestPath = $path . "/manifest.json";
+
+        $json = new JsonFile($manifestPath);
+        if (!$json->exists()) {
+            return null;
+        }
+        $manifest = $json->read();
+        if (!is_array($manifest)) {
+            return null;
+        }
+        $files = array();
+        if (isset($manifest["copy-from-recipe"]) && is_array($manifest["copy-from-recipe"])) {
+            foreach ($manifest["copy-from-recipe"] as $source => $destination) {
+                $directory = $path . "/" . trim($source, "\\/");
+                if (!file_exists($directory)) {
+                    continue;
+                }
+                if (is_dir($directory)) {
+                    $it = new \RecursiveDirectoryIterator($directory, \RecursiveDirectoryIterator::SKIP_DOTS);
+                    $ri = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::CHILD_FIRST);
+
+                    foreach ($ri as $file) {
+                        /** @var \SplFileInfo $file */
+                        if ($file->isFile()) {
+                            $filename = $file->getRealPath();
+
+                            $key = str_replace($path . "/", "", str_replace("\\", "/", $file));
+                            $files[$key] = [
+                                "contents" => file_get_contents($filename),
+                                "executable" => false,
+                            ];
+                        }
+                    }
+                } elseif (is_file($directory)) {
+                    $files[$source] = [
+                        "contents" => file_get_contents($path . "/" . $source),
+                        "executable" => false
+                    ];
+                }
+            }
+        }
+
+        return new Recipe($package, $name, $job, [
+            "repository" => "vendor", // ???
+            "package" => $name,
+            "version" => $version,
+            "manifest" => $manifest,
+            "files" => $files,
+            "origin" => sprintf('%s:%s@self-contain recipe', $name, $version),
+            "not_installable" => false,
+            "is_contrib" => false,
+        ]);
     }
 }
