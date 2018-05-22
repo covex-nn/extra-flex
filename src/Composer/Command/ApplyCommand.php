dae5b676744df1e347ee32f1dfb5f4287ee68d98
Christian Hammerl <info@christian-hammerl.de>
2018-05-22 22:14:32 +0200

    Remove extra output of the apply command

--- src/Composer/Command/ApplyCommand.php
+++ src/Composer/Command/ApplyCommand.php
@@ -75,7 +75,6 @@ class ApplyCommand extends BaseCommand
 
                 $recipe = RecipeHelper::createFromPath($package, $recipePath, "install");
                 if ($recipe instanceof Recipe) {
-                    $output->writeln("  - Applying recipe");
                     $this->plugin->applyRecipe($recipe);
                 } else {
                     $output->writeln("  - Recipe is not valid");
